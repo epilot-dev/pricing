ff403e1fee803639e37de177da2a6abb1413ce15
José Carneiro <j.carneiro@epilot.cloud>
2023-07-31 22:51:00 +0100

    Replace ternary chain by switch statement on item values computation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ import { d, toDinero } from '../formatters';
 import { Price, PriceTier, Tax } from '../types';
 
 type GetTaxValue = (tax: Tax) => number;
-type PriceItemsTotals = {
+export type PriceItemsTotals = {
   unitAmount?: number;
   unitAmountNet?: number;
   unitAmountGross?: number;

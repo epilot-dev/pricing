1f1030278709020ff09e05a7ca1ef07f11a454e4
Paulo Henriques <p.henriques@epilot.cloud>
2023-08-18 10:42:37 +0100

    Remove export

--- src/pricing.ts
+++ src/pricing.ts
@@ -339,11 +339,7 @@ export const computePriceDetails = (price: Price): PricingDetails => {
 /**
  * Computes all the pricing total amounts to integers with a decimal precision of DECIMAL_PRECISION.
  */
-export const recomputeDetailTotals = (
-  details: PricingDetails,
-  price: Price,
-  priceItemToAppend: PriceItem,
-): PricingDetails => {
+const recomputeDetailTotals = (details: PricingDetails, price: Price, priceItemToAppend: PriceItem): PricingDetails => {
   const taxes = details?.total_details?.breakdown?.taxes || [];
   const itemTax =
     priceItemToAppend.taxes?.[0]?.tax ||
@@ -566,7 +562,7 @@ const convertPriceComponentsPrecision = (items: PriceItem[], precision = 2): Pri
  * e.g: 10.00 with precision DECIMAL_PRECISION, represented as 10(+12 zeros) with precision 2
  * would be 1000(only 2 zeros on the decimal component).
  */
-export const convertPriceItemPrecision = (priceItem: PriceItem, precision = 2): PriceItem => ({
+const convertPriceItemPrecision = (priceItem: PriceItem, precision = 2): PriceItem => ({
   ...priceItem,
   ...(typeof priceItem.unit_amount === 'number' && {
     unit_amount: d(priceItem.unit_amount).convertPrecision(precision).getAmount(),
@@ -661,7 +657,7 @@ const getPriceRecurrence = (price: Price, recurrences: RecurrenceAmount[]) => {
   return recurrences.find((recurrenceItem) => recurrenceItem.type === 'one_time');
 };
 
-export const isUnitAmountApproved = (
+const isUnitAmountApproved = (
   priceItem: PriceItem,
   priceDisplayInJourneys?: Price['price_display_in_journeys'],
   parentPriceItem?: CompositePriceItem,

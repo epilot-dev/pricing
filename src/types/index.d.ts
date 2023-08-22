fb90c93f579732b04e9a1449dbec4268b0c6c51b
Juan Jose Guevara <juanjoseguevara@Juans-MacBook-Pro.local>
2023-08-22 15:20:18 +0200

    fix: changes import order of types now that is not importing from local file

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,5 @@
-import type { Dinero } from 'dinero.js';
-
 import { Components } from '@epilot/pricing-client';
+import type { Dinero } from 'dinero.js';
 
 export type Price = Components.Schemas.Price;
 export type PriceItem = Components.Schemas.PriceItem;

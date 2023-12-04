6fd483b9e5cb85fa8723c93d08ae2989daeb9e02
Diogo Fontainhas Garcia Marques <diogo@fontainhas.com>
2023-12-04 10:15:22 +0000

    fix: apply latest version of pricing api

--- src/pricing.ts
+++ src/pricing.ts
@@ -419,7 +419,7 @@ const recomputeDetailTotals = (details: PricingDetails, price: Price, priceItemT
       amount_subtotal_decimal: priceSubtotal.toUnit().toString(),
       amount_total_decimal: priceTotal.toUnit().toString(),
       amount_tax: priceTax.getAmount(),
-    } as any);
+    });
   } else {
     const unitAmountGrossAmount = d(recurrence.unit_amount_gross!);
     const subTotalAmount = d(recurrence.amount_subtotal);
@@ -428,8 +428,8 @@ const recomputeDetailTotals = (details: PricingDetails, price: Price, priceItemT
     recurrence.unit_amount_gross = unitAmountGrossAmount.add(priceUnitAmountGross).getAmount();
     recurrence.amount_subtotal = subTotalAmount.add(priceSubtotal).getAmount();
     recurrence.amount_total = totalAmount.add(priceTotal).getAmount();
-    (recurrence as any).amount_subtotal_decimal = subTotalAmount.add(priceSubtotal).toUnit().toString();
-    (recurrence as any).amount_total_decimal = totalAmount.add(priceTotal).toUnit().toString();
+    recurrence.amount_subtotal_decimal = subTotalAmount.add(priceSubtotal).toUnit().toString();
+    recurrence.amount_total_decimal = totalAmount.add(priceTotal).toUnit().toString();
     recurrence.amount_tax = taxAmount.add(priceTax).getAmount();
   }
 

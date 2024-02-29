175ad602f62b184fe3afda73beca93c58d6f5ee2
Paulo Henriques <p.henriques@epilot.cloud>
2024-02-29 17:58:54 +0000

    Remove cast

--- src/pricing.ts
+++ src/pricing.ts
@@ -662,7 +662,7 @@ export const computePriceItem = (
     ...priceItem,
     currency,
     ...(priceItemDescription && { description: priceItemDescription }),
-    ...(Number.isInteger(itemValues.unitAmount || 0) && { unit_amount: (itemValues as any)?.unitAmount || 0 }),
+    ...(Number.isInteger(itemValues.unitAmount || 0) && { unit_amount: itemValues?.unitAmount || 0 }),
     ...(Number.isInteger(itemValues.unitAmountNet) && { unit_amount_net: itemValues.unitAmountNet }),
     ...(price?.pricing_model === PricingModel.perUnit &&
       unitAmountDecimal && { unit_amount_decimal: unitAmountDecimal }),

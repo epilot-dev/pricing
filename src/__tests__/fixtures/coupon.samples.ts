d5746556afc03af1d5b24abbb5811a2474e3b9fd
José Carneiro <josemcarneiro@gmail.com>
2024-09-19 16:48:22 +0100

    Move coupon fixtures to standalone file (#99)
    Co-authored-by: José Carneiro <j.carneiro@epilot.cloud>

---
+++ src/__tests__/fixtures/coupon.samples.ts
@@ -0,0 +1,27 @@
+import type { Coupon } from '@epilot/pricing-client';
+
+export const percentageDiscountCoupon: Coupon = {
+  _id: 'coupon#1',
+  _schema: 'coupon',
+  _org: 'org#1',
+  _created_at: '2022-06-15T09:17:06.510Z',
+  _updated_at: '2022-06-17T11:48:20.104Z',
+  _title: 'Winter Sale',
+  name: 'Winter Sale',
+  type: 'percentage',
+  percentage_value: '25.5',
+};
+
+export const fixedDiscountCoupon: Coupon = {
+  _id: 'coupon#2',
+  _schema: 'coupon',
+  _org: 'org#1',
+  _created_at: '2022-06-15T09:17:06.510Z',
+  _updated_at: '2022-06-17T11:48:20.104Z',
+  _title: 'Winter Sale',
+  name: 'Winter Sale',
+  type: 'fixed',
+  fixed_value: 1000,
+  fixed_value_decimal: '10.00',
+  fixed_value_currency: 'EUR',
+};

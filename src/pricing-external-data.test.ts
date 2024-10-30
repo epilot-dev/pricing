12ca3c0074bb2792f38d63fd1c62f64531f039bc
Priit Pärna <priit.parna@epilot.cloud>
2024-10-30 13:13:38 +0200

    chore: fixed typos n stuff + added .idea to gitignore

--- src/pricing-external-data.test.ts
+++ src/pricing-external-data.test.ts
@@ -1,17 +1,12 @@
-
 import { computeAggregatedAndPriceTotals } from './pricing';
 import { pricesWithExternalData } from './__tests__/fixtures/price-external-data.samples';
 import { CompositePriceItem, PriceItem } from './types';
 
-
 describe('External Data - computeAggregatedAndPriceTotals', () => {
-  describe('whith internal and external prices', () => {
+  describe('with internal and external prices', () => {
     it('returns the correct totals and items', () => {
-      // given
-      const priceItems = pricesWithExternalData
-      
       // when
-      const result = computeAggregatedAndPriceTotals(priceItems);
+      const result = computeAggregatedAndPriceTotals(pricesWithExternalData);
       
       // then
       const computedItems = result.items;

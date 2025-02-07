63f8b63e03d23990b0675b9354ba9f3fa2e6fc53
José Carneiro <josemcarneiro@gmail.com>
2025-02-07 14:39:29 +0000

    Add tests for getTaxValue (#186)
    Co-authored-by: José Carneiro <j.carneiro@epilot.cloud>

--- src/pricing.test.ts
+++ src/pricing.test.ts
@@ -1,6 +1,7 @@
 import * as coupons from './__tests__/fixtures/coupon.samples';
 import * as samples from './__tests__/fixtures/price.samples';
 import * as results from './__tests__/fixtures/pricing.results';
+import * as taxes from './__tests__/fixtures/tax.samples';
 import {
   ENTITY_FIELDS_EXCLUSION_LIST,
   computeAggregatedAndPriceTotals,
@@ -12,7 +13,16 @@ import {
   mapToPriceSnapshot,
   mapToProductSnapshot,
 } from './pricing';
-import type { CompositePrice, CompositePriceItemDto, Price, PriceItemDto, PriceItems, PriceItemsDto } from './types';
+import type {
+  CompositePrice,
+  CompositePriceItemDto,
+  Price,
+  PriceItemDto,
+  PriceItems,
+  PriceItemsDto,
+  Tax,
+} from './types';
+import { getTaxValue } from './utils';
 
 describe('computeAggregatedAndPriceTotals', () => {
   describe('when is_composite_price = false', () => {
@@ -874,5 +884,34 @@ describe('getRecurrencesWithEstimatedPrices', () => {
 
     // then
     expect(result).toEqual({ monthly: true, one_time: true });
+  });
+});
+
+describe('getTaxValue', () => {
+  it('should return the tax value correctly', () => {
+    expect(getTaxValue(taxes.tax19percent)).toEqual(0.19);
+    expect(getTaxValue({ rate: 19.5 } as unknown as Tax)).toEqual(0.195);
+  });
+
+  it('should be permissive if tax object has rate as numeric string', () => {
+    expect(getTaxValue({ rate: '19' } as unknown as Tax)).toEqual(0.19);
+    expect(getTaxValue({ rate: ' 19' } as unknown as Tax)).toEqual(0.19);
+    expect(getTaxValue({ rate: '19 ' } as unknown as Tax)).toEqual(0.19);
+    expect(getTaxValue({ rate: ' 19 ' } as unknown as Tax)).toEqual(0.19);
+    expect(getTaxValue({ rate: '19.0' } as unknown as Tax)).toEqual(0.19);
+    expect(getTaxValue({ rate: ' 19.5 ' } as unknown as Tax)).toEqual(0.195);
+  });
+
+  it('should return 0 if not passed valid tax', () => {
+    expect(getTaxValue(undefined)).toEqual(0);
+    expect(getTaxValue(null as unknown as Tax)).toEqual(0);
+    expect(getTaxValue(NaN as unknown as Tax)).toEqual(0);
+    expect(getTaxValue({} as unknown as Tax)).toEqual(0);
+    expect(getTaxValue([] as unknown as Tax)).toEqual(0);
+    expect(getTaxValue({ rate: 'hello' } as unknown as Tax)).toEqual(0);
+    expect(getTaxValue({ rate: '19,5' } as unknown as Tax)).toEqual(0);
+    expect(getTaxValue({ rate: undefined } as unknown as Tax)).toEqual(0);
+    expect(getTaxValue({ rate: null } as unknown as Tax)).toEqual(0);
+    expect(getTaxValue({ rate: NaN } as unknown as Tax)).toEqual(0);
   });
 });

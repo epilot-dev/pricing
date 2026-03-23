b1c1af802b53782c6b544a955f47be061a6bd843
João Pinho <jpe.pinho@gmail.com>
2026-03-23 14:27:29 +0000

    update vite config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/pricing-playground/',
   plugins: [react()],
   resolve: {
     alias: {
       '@epilot/pricing': path.resolve(__dirname, '../src/index.ts'),
       '@epilot/pricing-client': path.resolve(__dirname, '../node_modules/@epilot/pricing-client'),
       'dinero.js': path.resolve(__dirname, '../node_modules/dinero.js'),
-      'i18next': path.resolve(__dirname, '../node_modules/i18next'),
+      i18next: path.resolve(__dirname, '../node_modules/i18next'),
     },
   },
   optimizeDeps: {

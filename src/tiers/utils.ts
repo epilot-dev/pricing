1514c97630066cfc8fe9f06897364f8cad82d33d
Paulo Henriques <paulo.henriques1993@gmail.com>
2026-04-23 12:40:53 +0100

    Fix: Improve Tiers Display Format

--- src/tiers/utils.ts
+++ src/tiers/utils.ts
@@ -144,7 +144,7 @@ export function getTierDescription(
     return;
   }
 
-  const { showStartsAt = true, enableSubunitDisplay = false, precision = 2 } = options;
+  const { showStartsAt = true, enableSubunitDisplay = false, precision } = options;
   const showUnitAmount =
     (pricingModel === PricingModel.tieredGraduated || pricingModel === PricingModel.tieredVolume) &&
     typeof tier.unit_amount === 'number';
@@ -161,7 +161,7 @@ export function getTierDescription(
     currency,
     locale,
     enableSubunitDisplay,
-    precision,
+    ...(precision !== undefined ? { precision } : { useRealPrecision: true }),
   };
 
   const unitAmountDecimal =
